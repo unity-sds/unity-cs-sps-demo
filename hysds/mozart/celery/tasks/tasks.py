8b99da36541be65b7b01e00b3cc1a64ab17ba7a5
buggtb <tom@spicule.co.uk>
2022-01-31 13:59:35 +0000

    update tasks endpointS

--- hysds/mozart/celery/tasks/tasks.py
+++ hysds/mozart/celery/tasks/tasks.py
@@ -3,7 +3,7 @@ import redis
 import msgpack
 
 app = Celery('tasks', broker='pyamqp://guest:guest@rabbitmq:5672//')
-r = redis.Redis(host="redis", port=6379, db=0)
+r = redis.Redis(host="redis-mozart.7c5iht.0001.use1.cache.amazonaws.com", port=6379, db=0)
 
 REDIS_LIST = "logstash"
 
