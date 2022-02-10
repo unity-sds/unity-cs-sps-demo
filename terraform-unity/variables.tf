8e2ce58feef461335ce0904ac9cd19d300b156d9
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2022-02-10 09:29:31 -0800

    updated with ami for us-west-2 region

--- terraform-unity/variables.tf
+++ terraform-unity/variables.tf
@@ -1,5 +1,6 @@
 variable "ami_name" { default = "unity-ubuntu" }
-variable "ami_id" { default = "ami-04505e74c0741db8d" }
+variable "ami_id" { default = "ami-63cb0503" }
+#variable "ami_id" { default = "ami-04505e74c0741db8d" }
 variable "ami_key_pair_name" { default = "barber-unity-pair" }
 variable "vpc_id" { default = "vpc-0fd177185e450d73d" }
 
