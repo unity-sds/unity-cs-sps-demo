14a017b4cc0484bc0f03d1966829aa799fbb1206
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2022-02-10 09:39:52 -0800

    added a new key pair for this region

--- terraform-unity/variables.tf
+++ terraform-unity/variables.tf
@@ -1,7 +1,7 @@
 variable "ami_name" { default = "unity-ubuntu" }
 variable "ami_id" { default = "ami-7d482305" }
 #variable "ami_id" { default = "ami-04505e74c0741db8d" }
-variable "ami_key_pair_name" { default = "barber-unity-pair" }
+variable "ami_key_pair_name" { default = "unity-cs-smolensk" }
 variable "vpc_id" { default = "vpc-0fd177185e450d73d" }
 
 data "aws_vpc" "unity-test-env" {
