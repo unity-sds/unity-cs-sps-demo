41b1c1edd7eaf4820f69d1eeac6bbe86913faac2
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2022-04-12 15:03:13 -0700

    reverting back to JPL AWS config

--- terraform-unity/variables.tf
+++ terraform-unity/variables.tf
@@ -2,9 +2,10 @@ variable "ami_name" { default = "unity-ubuntu" }
 variable "ami_id" { default = "ami-0688ba7eeeeefe3cd" }
 #variable "ami_id" { default = "ami-04505e74c0741db8d" }
 variable "ami_key_pair_name" { default = "unity-cs-smolensk" }
-#variable "vpc_id" { default = "vpc-0fd177185e450d73d" }
+variable "vpc_id" { default = "vpc-0106218dbddd3a753" }
 
 data "aws_vpc" "unity-test-env" {
-  id         = "vpc-0106218dbddd3a753"
+  id         = var.vpc_id
   cidr_block = "10.0.0.0/16"
-}
+} 
+
