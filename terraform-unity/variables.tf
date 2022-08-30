e88ab10e700eb8fa948081fbb057e44f8c78a5bf
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2022-08-30 09:54:24 -0700

    referring to tfvars file for ami id and vpc id

--- terraform-unity/variables.tf
+++ terraform-unity/variables.tf
@@ -1,11 +1,11 @@
 variable "ami_name" { default = "unity-ubuntu" }
-variable "ami_id" { default = "ami-0688ba7eeeeefe3cd" }
+variable "ami_id" { default = var.AMI_ID }
 #variable "ami_id" { default = "ami-04505e74c0741db8d" }
 variable "ami_key_pair_name" { default = "unity-cs-smolensk" }
 #variable "vpc_id" { default = "vpc-0fd177185e450d73d" }
 
 data "aws_vpc" "unity-test-env" {
-  id         = "vpc-0106218dbddd3a753"
+  id         = var.VPC_ID
   cidr_block = "10.0.0.0/16"
 }
 
